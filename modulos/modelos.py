4489a0733ef0ef1a4898ab1857f682755f288c2c
Natã do Carmo Souza <nata.carmo05@gmail.com>
2025-10-03 02:09:50 -0300

    Modelos de Dados (Dataclasses)
    Definição da estrutura de dados central do sistema no arquivo modulos/modelos.py por meio de dataclasses. As classes para todas as 7 entidades do projeto foram criadas, incluindo Chaves Estrangeiras (FKs) e todos os campos necessários para as Regras de Negócio (como IMC e controle de Diárias), estabelecendo a fundação para a lógica de I/O de arquivos e de serviços.

---
+++ modulos/modelos.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+
+@dataclass
+class Cidade:
+    cod_cidade: int
+    descricao: str
+    estado: str
+
+@dataclass
+class Paciente:
+    cod_paciente: int
+    nome: str
+    data_nascimento: str
+    endereco: str
+    telefone: str
+    cod_cidade: int
+    peso: float
+    altura: float
+
+@dataclass
+class Especialidade:
+    cod_especialidade: int
+    descricao: str
+    valor_consulta: float
+    limite_diario: int
+
+@dataclass
+class Medico:
+    cod_medico: int
+    nome: str
+    endereco: str
+    telefone: str
+    cod_cidade: int
+    cod_especialidade: int
+
+@dataclass
+class Exame:
+    cod_exame: int
+    descricao: str
+    cod_especialidade: int
+    valor_exame: float
+
+@dataclass
+class Consulta:
+    cod_consulta: int
+    cod_paciente: int
+    cod_medico: int
+    cod_exame: int
+    data: str
+    hora: str
+
+@dataclass
+class Diaria:
+    cod_dia: str
+    cod_especialidade: int
+    quantidade_consultas: int
