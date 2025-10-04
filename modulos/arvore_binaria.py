1501d9baffa5a6d9c8f99f43482844888e128109
Natã do Carmo Souza <nata.carmo05@gmail.com>
2025-10-04 10:41:58 -0300

    Método Exclusão (Remover)
    Implementação do método de exclusão na BST, tratando os casos de remoção (nó folha, um filho e dois filhos) para manter a integridade da árvore após uma exclusão de registro.

--- modulos/arvore_binaria.py
+++ modulos/arvore_binaria.py
@@ -43,3 +43,41 @@ class ArvoreBinaria:
             return self._buscar_recursivo(no_atual.esquerda, chave)
         else:
             return self._buscar_recursivo(no_atual.direita, chave)
+
+    def remover(self, chave):
+        if self.raiz is None:
+            return False
+        self.raiz = self._remover_recursivo(self.raiz, chave)
+        return True
+
+    def _remover_recursivo(self, no_atual, chave):
+        if no_atual is None:
+            return no_atual
+
+        if chave < no_atual.chave:
+            no_atual.esquerda = self._remover_recursivo(no_atual.esquerda, chave)
+            return no_atual
+        elif chave > no_atual.chave:
+            no_atual.direita = self._remover_recursivo(no_atual.direita, chave)
+            return no_atual
+
+        else:
+            if no_atual.esquerda is None:
+                return no_atual.direita
+            elif no_atual.direita is None:
+                return no_atual.esquerda
+
+            sucessor = self._encontrar_minimo(no_atual.direita)
+
+            no_atual.chave = sucessor.chave
+            no_atual.num_linha = sucessor.num_linha
+
+            no_atual.direita = self._remover_recursivo(no_atual.direita, sucessor.chave)
+
+            return no_atual
+
+    @staticmethod
+    def _encontrar_minimo(no_atual):
+        while no_atual.esquerda is not None:
+            no_atual = no_atual.esquerda
+        return no_atual
